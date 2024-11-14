36093d679120baf5c9dead8371c60a93760eb57a
Ami <ami.shah@cumminscollege.in>
2024-11-14 17:53:08 +0530

    Navbar signin signout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 // src/components/Navbar.js
-// src/components/Navbar.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'; 
@@ -8,12 +8,13 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
+        <img src="../assets/image"></img>
         <Link to="/">Freelance Hub</Link> {/* Logo text, can be replaced with an actual logo */}
       </div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/discover">Discover</Link></li>
         <li><Link to="/skill-development">Skill Development</Link></li>
       </ul>
       <div className="navbar-auth">
