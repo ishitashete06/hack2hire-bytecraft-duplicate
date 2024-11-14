512ef18433ec8d2a0eeb0628ff9bb0683116cc85
Ami <ami.shah@cumminscollege.in>
2024-11-14 16:53:36 +0530

    navbar improved

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,5 @@
 // src/components/Navbar.js
+// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'; 
@@ -6,13 +7,19 @@ import '../styles/Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
+      <div className="navbar-logo">
+        <Link to="/">Freelance Hub</Link> {/* Logo text, can be replaced with an actual logo */}
+      </div>
+      <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/skill-development">Skill Development</Link></li>
-        {/* Add more links as needed */}
       </ul>
+      <div className="navbar-auth">
+        <Link to="/signin" className="btn btn-signin">Sign In</Link>
+        <Link to="/signup" className="btn btn-signup">Sign Up</Link>
+      </div>
     </nav>
   );
 }
