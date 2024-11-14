00a83e5e6e947574952589bf9a51e1d5af1c691d
Ami Shah <127313122+amishah1110@users.noreply.github.com>
2024-11-14 12:08:47 +0530

    Update Discover.js

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -1,14 +1,18 @@
 // src/pages/Discover.js
 import React from 'react';
 import '../styles/Discover.css';
+import { useNavigate } from 'react-router-dom';
 
 function Discover() {
+  const navigate = useNavigate();
   return (
     <div className="discover">
-      <h1>Discover Categories of Projects</h1>
+      <h1>Discover Courses and Projects</h1>
       <div className="courses">
-        <div className="course-card">Web Development</div>
-        <div className="course-card">Graphic Design</div>
+        <button className="course-card" onClick={()=> navigate('/swipe-project')}>Projects</button> 
+      {/* develop the swipe functionality here in Projects button */}
+        <button className="course-card" onClick={()=> navigate('/skill-development')}>Courses</button>
+        
       </div>
     </div>
   );
