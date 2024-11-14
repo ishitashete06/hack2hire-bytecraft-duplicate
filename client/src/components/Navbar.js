d23a4b68960549bdcd79566915247ee15f52b193
Ishita Shete <ishitashete06@gmail.com>
2024-11-14 16:49:33 +0530

    assets folder added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
     <nav className="navbar">
       <ul>
         <li><Link to="/">Home</Link></li>
+        
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/skill-development">Skill Development</Link></li>
